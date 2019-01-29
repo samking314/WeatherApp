1588fd1e02ca68a2cd156d81ff585d7903b0bb32
samking <sking314@bu.edu>
2019-01-29 18:13:52 -0500

    Initial Commit

---
+++ WeatherApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeatherApp
+//
+//  Created by Sam King on 1/29/19.
+//  Copyright © 2019 Sam King. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WeatherApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WeatherApp
+//
+//  Created by Sam King on 1/29/19.
+//  Copyright © 2019 Sam King. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
